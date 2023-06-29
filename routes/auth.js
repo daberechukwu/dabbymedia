8735d19bdd69b6d1e7ea91fb097c7f4149546f4a
dabere <daberechukwu18@gmail.com>
2023-06-29 11:44:21 +0100

    third commit

--- routes/auth.js
+++ routes/auth.js
@@ -29,7 +29,9 @@ router.post("/register", async(req, res)=>{
 
 router.post("/login", async(req, res)=>{
   try {
-    const user = await User.findOne({ email: req.body.email });
+    const user = await User.findOne({ 
+      email: req.body.email 
+    });
     !user && res.status(404).json("user not found");
 
     const validPassword = await bcrypt.compare(req.body.password, user.password)
